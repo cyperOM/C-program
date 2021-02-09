542898a677955b46c71a6b08ebdc5ed006336c6a
Mohammed Said Al Busaidi <70335350+cyperOM@users.noreply.github.com>
2021-02-09 16:10:38 -0500

    Playing

--- PrintList.c
+++ PrintList.c
@@ -1,14 +1,54 @@
 #include <stdio.h>
-int main() {
+
+
+void reed();
+void resetcolor();
+
+void main() {
+
+
+
+int i;
+for ( i = 0; i < 7; i++)
+{
+
+    
+
+}
 
 
 	char help_list[][20] = {"version","help","shutdown","getdate","setdate","gettime","settime"};
+	char help_instructions[][90] = {"this will print the version ","this will bring the list","close system ","formate xx/xx/xxxx","setdate xx/xx/xxxx","gettime xx/xx/xx","settime xx/xx/xx"};
+ int buffersize;
 
-for (int i = 0; i < 7; ++i)
+int i;
+for ( i = 0; i < 7; i++)
 {
 
-    printf("%s\n",help_list[i]);
-}
-return 0;
+    switch (i)
+    {
+    case 3:
+        reed();
+        break;
+    
+    default:
+    resetcolor();
+        break;
+    }
+  buffersize=99;
+	sys_req(WRITE,DEFULT_DEVICE,help_list[i],&buffersize);
+    buffersize=99;
 
+  	sys_req(WRITE,DEFULT_DEVICE,help_instructions[i],&buffersize);
+}
+
+}
+
+void reed () {
+  printf("\033[1;31m");
+}
+
+
+void resetcolor () {
+  printf("\033[0m");
 }
---
+++ linkedlist.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+typedef struct List
+{
+  char *Name;
+  int number;
+  struct List *next;  
+}List;
+
+
+void main(){
+    List *data;
+    data = NULL;
+    data = malloc(sizeof(List));
+
+    data -> letter =  
+
+}
